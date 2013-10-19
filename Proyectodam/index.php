75fd94ba3fcf37907aaf6f57c545ff47622f1ea2
Jaume <dimoni666@gmail.com>
2013-10-19 18:28:03 +0200

    Arreglando Uploader( redirreccionando)

--- Proyectodam/index.php
+++ Proyectodam/index.php
@@ -24,12 +24,27 @@
             <li><a href="../mediaplayer/reproductor.html" target="_blank">Reproductor</a></li>
 		</ul>
 		</div>
-	<div id="contenido">
-    <h1> La historia del Rap </h1>
+		<div id="contenido">
+		<?php
+			if (isset($_GET["resultado"]) and ($_GET["resultado"] == "exito")) {
+		?>
+				<h1>Fichero subido con éxito!</h1>
+		<?php	
+			}elseif (isset($_GET["resultado"]) and ($_GET["resultado"] == "fallo")){
+		?>
+			<h1>No se ha podido subir el archivo</h1>
+			<p>Las extensiones que aceptamos son: .rar,.jpg,.png,.gif y .mp3</p>
+		<?php
+			}else{
+		?>
+		<h1> La historia del Rap </h1>
 		<p>El rap surge como un estilo musical en los barrios negros y latinos de Nueva York en la década de 1970, como la expresión desde el sonido y las melodías de la cultura hip hop, que agrupa corrientes estéticas como el graffiti, el breakdance o el scratch. Desde allí se lanzó al mundo, tomando diferentes peculiaridades regionales en cada país o continente.
-Musicalmente hablando, mientras el hip hop propiamente dicho nace desde la música religiosa de origen negro y se funde en gran manera con el funk y el disco, el rap rompe con esta ligazón y se acerca más al breakdance, tomando al mismo tiempo un lenguaje propio.
-En las primeras presentaciones de grupos de rap, el estilo se sustentaba en las musicas de un Disc Jockey que, utilizando algunas canciones y empleando cortes y mezclas, lograba una composición que funcionaba como fondo sonoro para que pudiese rapear un MC o varios intérpretes al mismo tiempo.
-Ese lenguaje y esa cadencia particular se sustentan en una actitud contestataria, de una clara tendencia anti-sistema. Pero como toda expresión artística, resulta muy complejo detenerla o congelarla. Es así que los raperos de los años 70 dieron lugar a una expresión más metafórica y compleja desde la lírica en los años 80, que a la vez se manifestó en los ritmos y bases de musica empleadas.</p>
+				Musicalmente hablando, mientras el hip hop propiamente dicho nace desde la música religiosa de origen negro y se funde en gran manera con el funk y el disco, el rap rompe con esta ligazón y se acerca más al breakdance, tomando al mismo tiempo un lenguaje propio.
+				En las primeras presentaciones de grupos de rap, el estilo se sustentaba en las musicas de un Disc Jockey que, utilizando algunas canciones y empleando cortes y mezclas, lograba una composición que funcionaba como fondo sonoro para que pudiese rapear un MC o varios intérpretes al mismo tiempo.
+				Ese lenguaje y esa cadencia particular se sustentan en una actitud contestataria, de una clara tendencia anti-sistema. Pero como toda expresión artística, resulta muy complejo detenerla o congelarla. Es así que los raperos de los años 70 dieron lugar a una expresión más metafórica y compleja desde la lírica en los años 80, que a la vez se manifestó en los ritmos y bases de musica empleadas.</p>
+		<?php
+			}
+		?>		
 	</div>
     <div id="sesion">
     <?php
