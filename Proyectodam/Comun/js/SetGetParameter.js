f0e859338a942d033137c96791c067d003db0a04
dimoni666 <dimoni666@gmail.com>
2014-03-08 19:56:09 +0100

    Probando JS

---
+++ Proyectodam/Comun/js/SetGetParameter.js
@@ -0,0 +1,20 @@
+function SetGetParameter(paramName, paramValue)
+{
+    var url = window.location.href;
+    if (url.indexOf(paramName + "=") >= 0)
+    {
+        var prefix = url.substring(0, url.indexOf(paramName));
+        var suffix = url.substring(url.indexOf(paramName)).substring(url.indexOf("=") + 1);
+        suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+        url = prefix + paramName + "=" + paramValue + suffix;
+    }
+    else
+    {
+    if (url.indexOf("?") < 0)
+        url += "?" + paramName + "=" + paramValue;
+    else
+        url += "&" + paramName + "=" + paramValue;
+    }
+    window.location.href = url;
+}
+
