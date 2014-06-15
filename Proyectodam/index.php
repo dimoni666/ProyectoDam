2a4a42fa60b4d5a461a6f69980e6c61f7960e315
dimoni666 <dimoni666@gmail.com>
2014-06-15 23:41:48 +0200

    descarga arreglada

--- Proyectodam/index.php
+++ Proyectodam/index.php
@@ -78,7 +78,7 @@
     	if (isset($_SESSION["autenticado"]) and ($_SESSION["autenticado"] == "SI")) {
     		echo $idioma->contenido->logged->saludo.", " . $_SESSION["nombre"] . "<br/>";
     		echo "<a href='Registro/cerrarSesion.php'>".$idioma->contenido->logged->cerrar."</a>";
-    		echo "<form><input type=\"button\" value=\"Descarga\" onClick=\"window.location.href='../Comun/Descargas/setup.sfx.exe'\"></form>";
+    		echo "<form><input type=\"button\" value=\"Descarga\" onClick=\"window.location.href='../Descargas/setup.sfx.exe'\"></form>";
     	}else {
 			echo "<a href='Registro/index.php'>".$idioma->contenido->logged->iniciar."</a>";
     	}
